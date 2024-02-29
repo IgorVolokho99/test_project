276cda8b69a54df1d8ecc038f2b3e7730329bc4d
Igor Volokho <igorvolokho99@gmail.com>
2024-03-01 02:56:36 +0300

    Second commit(Correct). Added some new pages and css styles.

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
-menu = ["Установка", "Первое приложение", "Обратная связь"]
+# menu = ["Установка", "Первое приложение", "Обратная связь"]
+
+menu = [{"name": "Установка", "url": "install-flask"},
+        {"name": "Первое приложение", "url": "first-app"},
+        {"name": "Обратная связь", "url": "contact"}]
 
 
 @app.route("/index")
@@ -14,6 +18,13 @@ def index():
 @app.route("/about")
 def about():
     return render_template("about.html", title="О сайте", menu=menu)
+
+
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    if request.method == "POST":
+        print(request.form)
+    return render_template("contact.html", title="Обратная связь", menu=menu)
 
 
 if __name__ == "__main__":
