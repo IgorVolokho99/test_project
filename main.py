a51e7a497987c01ba3d888b750774b44e9c295f2
Igor Volokho <igorvolokho99@gmail.com>
2024-03-01 14:20:17 +0300

    Third commit.Add flash messages.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, flash
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "You will never guess my secret key"
 
 # menu = ["Установка", "Первое приложение", "Обратная связь"]
 
@@ -23,7 +24,10 @@ def about():
 @app.route("/contact", methods=["POST", "GET"])
 def contact():
     if request.method == "POST":
-        print(request.form)
+        if len(request.form["username"]) > 2:
+            flash("Сообщение отправлено", category="success")
+        else:
+            flash("Ошибка отправки", category="error")
     return render_template("contact.html", title="Обратная связь", menu=menu)
 
 
